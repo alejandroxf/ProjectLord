522f83ac17ef26ddac12089a982a1c0db9128a7b
osktgui <osktgui@gmail.com>
2013-07-15 23:16:11 -0500

    Structure

---
+++ LordLima/Entity/User.h
@@ -0,0 +1,15 @@
+//
+//  User.h
+//  LordLima
+//
+//  Created by mac developer on 7/15/13.
+//  Copyright (c) 2013 Alejandro Mayta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+@property(nonatomic, strong) NSString *Name;
+@property(nonatomic, strong) NSString *Password;
+@property(nonatomic, strong) NSString *Email;
+@end
---
+++ LordLima/Entity/User.m
@@ -0,0 +1,15 @@
+//
+//  User.m
+//  LordLima
+//
+//  Created by mac developer on 7/15/13.
+//  Copyright (c) 2013 Alejandro Mayta. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+@synthesize Name;
+@synthesize Email;
+@synthesize Password;
+@end
