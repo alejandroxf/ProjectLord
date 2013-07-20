ab8561b3d394507ff577ee540ff70dcdcc6ddbe1
osktgui <osktgui@gmail.com>
2013-07-20 13:32:34 -0500

    Audio v.3

---
+++ LordLima/Lib/Audio/XFAudioSession.h
@@ -0,0 +1,13 @@
+//
+//  XFAudioSession.h
+//  LordLima
+//
+//  Created by mac developer on 7/19/13.
+//  Copyright (c) 2013 Alejandro Mayta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XFAudioSession : NSObject
+
+@end
---
+++ LordLima/Lib/Audio/XFAudioSession.m
@@ -0,0 +1,13 @@
+//
+//  XFAudioSession.m
+//  LordLima
+//
+//  Created by mac developer on 7/19/13.
+//  Copyright (c) 2013 Alejandro Mayta. All rights reserved.
+//
+
+#import "XFAudioSession.h"
+
+@implementation XFAudioSession
+
+@end
